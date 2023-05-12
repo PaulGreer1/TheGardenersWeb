ea59ed44d52b8379b6d8ef2004db60fa1c62be04
PaulGreer1 <130761825+PaulGreer1@users.noreply.github.com>
2023-05-12 12:45:53 +0100

    Add files via upload

---
+++ src/main/java/com/strongcoffee/tgw/Controller.java
@@ -0,0 +1,133 @@
+package com.strongcoffee.tgw;
+
+import java.util.Map;
+
+/**
+ *
+ */
+interface EventHandler {
+	/**
+	 * @param input .
+	 * @return ...
+	 */
+	boolean execute( Map<String, String> input );
+}
+
+/**
+ * References to these lambda functions are stored with the Registrar object. Each handler is registered for a set of events. When an
+ * event for which a handler is registered for occurs, the Registrar will call the handler.
+ */
+public class Controller {
+
+	public Controller( Registrar registrar ) {
+
+		String[] events = { "INSERT_GARDEN", "UPDATE_GARDEN", "DELETE_GARDEN", "GET_GARDEN", "GET_GARDENS" };
+
+		// Register event handlers to be called back when events occur.
+		registrar.add( insertGarden, "1", events[0] );
+	}
+
+	/**
+	 *
+	 */
+	EventHandler insertGarden = (input) -> {
+
+		System.out.println( input.get("ui_event") + " | " + input.get("garden_name") + " | " + input.get("description") );
+
+		// Insert a Garden into the database.
+		// ...
+
+		return true;
+	};
+
+	/**
+	 *
+	 */
+	EventHandler updateGarden = (input) -> {
+
+		System.out.println( input + " Bill." );
+
+		// Update a Garden in the database.
+		// ...
+
+		return true;
+	};
+
+	/**
+	 *
+	 */
+	EventHandler deleteGarden = (input) -> {
+
+		System.out.println( input + " Carrol." );
+
+		// Delete a Garden from the database.
+		// ...
+
+		return true;
+	};
+
+	/**
+	 *
+	 */
+	EventHandler getGarden = (input) -> {
+
+		System.out.println( input + " Dodo." );
+
+		// Get a Garden from the database.
+		// ...
+
+		return true;
+	};
+
+	/**
+	 *
+	 */
+	EventHandler getGardens = (input) -> {
+
+		System.out.println( input + " Mad Hatter." );
+
+		// Get a set of Gardens from the database.
+		// ...
+
+		return true;
+	};
+}
+
+/*
+JDBC PreparedStatement tutorial ( https://jenkov.com/tutorials/jdbc/preparedstatement.html )
+
+Executing a PreparedStatement looks like executing a regular Statement. To execute a query, call the executeQuery() or executeUpdate method.
+
+An executeQuery() example:
+
+String sql = "SELECT * FROM people WHERE firstname = ? AND lastname = ?";
+
+PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+preparedStatement.setString( 1, "John" );
+preparedStatement.setString( 2, "Smith" );
+
+ResultSet result = preparedStatement.executeQuery();
+
+As you can see, the executeQuery() method returns a ResultSet.
+
+An executeUpdate() example:
+
+String sql = "UPDATE people SET firstname = ?, lastname = ? WHERE id = ?";
+
+PreparedStatement preparedStatement = connection.prepareStatement( sql );
+
+preparedStatement.setString( 1, "Gary" );
+preparedStatement.setString( 2, "Larson" );
+preparedStatement.setLong  ( 3, 123 );
+
+int rowsAffected = preparedStatement.executeUpdate();
+
+Prepared statements can be reused:
+
+preparedStatement.setString(1, "Stan");
+preparedStatement.setString(2, "Lee");
+preparedStatement.setLong  (3, 456);
+
+int rowsAffected = preparedStatement.executeUpdate();
+ */
---
+++ src/main/java/com/strongcoffee/tgw/Handler.java
@@ -0,0 +1,67 @@
+package com.strongcoffee.tgw;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Map;
+
+/**
+ *
+ */
+public class Handler implements RequestHandler< APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse > {
+
+	String url = "jdbc:mysql://localhost:3306/TheGardenersWeb";
+	String username = "root";
+	String password = "";
+	Statement statement;
+	ResultSet resultSet;
+	String currentTime = "";
+	Connection con;
+	StringBuilder stringBuilder;
+	Registrar registrar = new Registrar();
+	Controller controller = new Controller( registrar );
+
+	/**
+	 * @param event   The Lambda Function input. Includes input about the UI event which occurred. This will determine which
+	 *                event handlers will be invoked.
+	 * @param context The Lambda execution environment context object.
+	 * @return .
+	 */
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest( APIGatewayV2HTTPEvent event, Context context ) {
+		Map<String, String> queryStringParameters = event.getQueryStringParameters();
+		String uiEvent = queryStringParameters.get( "ui_event" );
+		System.out.println( uiEvent );
+		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+		stringBuilder = new StringBuilder();
+		registrar.setQueryStringParameters( queryStringParameters );          // Set the Registrar's queryStringParameters member variable.
+		registrar.notify( uiEvent );
+
+		try {
+			con = DriverManager.getConnection( url, username, password );
+			statement = con.createStatement();
+			resultSet = statement.executeQuery( "SELECT * FROM garden" );
+
+			while( resultSet.next() ) {
+				stringBuilder.append(resultSet.getObject("garden_name").toString());
+				stringBuilder.append(" | ");
+				stringBuilder.append(resultSet.getObject("description").toString());
+				stringBuilder.append("\n");
+			}
+			con.close();
+			response.setBody( stringBuilder.toString() );
+		}
+		catch ( Exception e ) {
+			response.setBody( "Exception: " + e.getMessage() + "\nCause: " + e.getCause() );
+		}
+
+		System.out.println( response.getBody() );
+
+		return response;
+	}
+}
