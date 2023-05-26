4ac5a04b717f4ba94a91ab31a3d397bd71f4a8cd
PaulGreer1 <130761825+PaulGreer1@users.noreply.github.com>
2023-05-26 18:48:10 +0100

    Add files via upload

---
+++ src/main/java/com/strongcoffee/tgw/Gateway.java
@@ -0,0 +1,88 @@
+package com.strongcoffee.tgw;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+/**
+ * Simulate an HTTP API Gateway. Send the APIGatewayV2HTTPEvent HashMap<String, String> queryStringParameters with some
+ * UI data that will cause registered event handlers to be notified and invoked.
+ */
+public class Gateway {
+
+	/**
+	 *
+	 */
+	static Map<String, String> queryStringParameters;
+	static APIGatewayV2HTTPEvent event = new APIGatewayV2HTTPEvent();
+	static APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+	static Context context = null;
+	static Handler handler = new Handler();
+
+	public static void main( String[] args ) {
+
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "GET_GARDENS" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+		response = handler.handleRequest( event, context );
+
+/*
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Alice's garden" );
+		queryStringParameters.put( "description", "The perfect place for a very happy unbirthday." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Bill's garden" );
+		queryStringParameters.put( "description", "A garden for lazy lizards." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Carroll's garden" );
+		queryStringParameters.put( "description", "As many interpretations as there are people." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "David's garden" );
+		queryStringParameters.put( "description", "A small space with big diversity." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+*/
+
+/*
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "DELETE_GARDEN" );
+		queryStringParameters.put( "garden_id", "5" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+*/
+
+/*
+		// ""  "asfd"  "a,3,4"  "1,4"  "5"
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "DELETE_GARDENS" );
+		queryStringParameters.put( "garden_ids", "1,2,8" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+*/
+
+		System.out.println( response.getBody() );
+	}
+}
+
+/*
+SELECT * FROM garden;
+
+DELETE FROM garden WHERE garden_id IN (1,2,7,8);
+
+*/
