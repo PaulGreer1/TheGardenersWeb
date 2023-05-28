ce67d4a802c11f350998a011095a33cd0c6f8002
PaulGreer1 <130761825+PaulGreer1@users.noreply.github.com>
2023-05-28 10:12:06 +0100

    Add files via upload

---
+++ src/main/java/com/strongcoffee/tgw/Gateway.java
@@ -0,0 +1,105 @@
+package com.strongcoffee.tgw;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+/**
+ * Simulate an HTTP API Gateway. Send the APIGatewayV2HTTPEvent HashMap<String, String> queryStringParameters with some
+ * UI data that will cause registered event handlers to be notified and invoked.
+ */
+public class Gateway {
+
+	/**
+	 *
+	 */
+	static Map<String, String> queryStringParameters;
+	static APIGatewayV2HTTPEvent event = new APIGatewayV2HTTPEvent();
+	static APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+	static Context context = null;
+	static Handler handler = new Handler();
+
+	public static void main( String[] args ) {
+
+		// Some sample requests.
+
+		// The following request gets all the gardens in the database. A string
+		// is returned. The client must split this string on the hash ('#') and
+		// pipe ('|') characters to get at the data.
+
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "GET_GARDENS" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+		response = handler.handleRequest( event, context );
+
+/******************************************************
+		// The following requests insert gardens into the database. They can be
+        // run all at once or one at a time.
+
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Alice's garden" );
+		queryStringParameters.put( "description", "The perfect place for a very happy unbirthday." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Bill's garden" );
+		queryStringParameters.put( "description", "A garden for lazy lizards." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "Carroll's garden" );
+		queryStringParameters.put( "description", "As many interpretations as there are people." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+		//queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "INSERT_GARDEN" );
+		queryStringParameters.put( "garden_name", "David's garden" );
+		queryStringParameters.put( "description", "A small space with big diversity." );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+
+******************************************************/
+
+/******************************************************
+		// The following request deletes a garden from the database.
+
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "DELETE_GARDEN" );
+		queryStringParameters.put( "garden_id", "5" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+******************************************************/
+
+/******************************************************
+		// The following request deletes a set of gardens from the database. The
+        // garden_ids value passed to queryStringParameters.put() method is a set
+        // of comma-separated integers. The event handler for this request will
+        // accept only commas and integers.
+
+		// ""  "asfd"  "a,3,4"  "1,4"  "5"
+		queryStringParameters = new HashMap<String, String>();
+		queryStringParameters.put( "ui_event", "DELETE_GARDENS" );
+		queryStringParameters.put( "garden_ids", "1,2,8" );
+		event.setQueryStringParameters( queryStringParameters );
+		response = handler.handleRequest( event, context );
+******************************************************/
+
+		System.out.println( response.getBody() );
+	}
+}
+
+/*
+SELECT * FROM garden;
+
+DELETE FROM garden WHERE garden_id IN (1,2,7,8);
+
+*/
---
+++ src/main/java/com/strongcoffee/tgw/Handler.java
@@ -0,0 +1,53 @@
+package com.strongcoffee.tgw;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+/**
+ *
+ */
+public class Handler implements RequestHandler< APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse > {
+
+	/*
+	The following can be initialised/instantiated before handleRequest() is invoked because they don't rely on
+	input from the gateway.
+	*/
+	Registrar registrar = new Registrar();
+	Controller controller = new Controller( registrar );
+
+	/**
+	 * @param event   The Lambda Function input. Includes input about the UI event which occurred. This will determine which
+	 *                event handlers will be invoked.
+	 * @param context The Lambda execution environment context object.
+	 * @return .
+	 */
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest( APIGatewayV2HTTPEvent event, Context context ) {
+
+		System.out.println( registrar.queryStringParameters );
+
+		Map<String, String> queryStringParameters = event.getQueryStringParameters();
+		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+
+		// Set the Registrar's queryStringParameters member variable.
+		registrar.setQueryStringParameters( queryStringParameters );
+
+		// Notify registered event handlers of the event.
+		registrar.notify( queryStringParameters.get( "ui_event" ) );
+
+		// Build a string to send back to the Gateway.
+		StringBuilder output = new StringBuilder();
+		for( HashMap.Entry< String, String > entry : this.registrar.dataStore.entrySet() ) {
+			output.append( entry.getKey() );
+			output.append( "#" );
+			output.append( this.registrar.dataStore.get( entry.getKey() ) );
+		}
+		response.setBody( output.toString() );
+
+		return response;
+	}
+}
